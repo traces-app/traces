c7cfd49c714e5e7e8154c89ae3f789470f3f21b2
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2025-01-01 00:27:37 +0530

    added custom back button in the layout file to trigger on pages that requires functionality to visit backwards

--- lib/pages/authentication/layouts/layout.dart
+++ lib/pages/authentication/layouts/layout.dart
@@ -7,16 +7,34 @@ class AuthenticationLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-      ),
       backgroundColor: Colors.black,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        child: Column(
-          children: [
-            ...(children ?? []),
-          ],
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Stack(
+            children: [
+              /*
+              Positioned(
+                right: 22.0,
+                top: 6.0,
+                child: Icon(
+                  Icons.arrow_back,
+                  color: Colors.white.withOpacity(0.8),
+                  size: 25.5,
+                ),
+              ),
+              */
+              Padding(
+                padding: const EdgeInsets.only(left: 25, right: 25, top: 30),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ...(children ?? []),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
