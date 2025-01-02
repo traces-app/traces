f8069cb5d98632e79cf151c634916ea49b990b9b
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2025-01-02 11:45:16 +0530

    setted up firebase google authentication

---
+++ lib/core/utils/app_icons.dart
@@ -0,0 +1,8 @@
+class AppIcons {
+  AppIcons._();
+
+  static const String google = 'assets/icons/google.svg';
+  static const String apple = 'assets/icons/apple.svg';
+
+  // add more icons as needed...
+}
