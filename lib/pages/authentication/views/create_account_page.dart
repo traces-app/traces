db0d89105d49a89fe5eb179678768a1e38b71c5c
rashini <rashiniruwindee2001@gmail.com>
2025-01-26 17:56:52 +0530

    TCS-11: Updated create account page

--- lib/pages/authentication/views/create_account_page.dart
+++ lib/pages/authentication/views/create_account_page.dart
@@ -1,57 +1,132 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:traces/pages/authentication/authentication.dart';
+import 'package:traces/pages/authentication/cubit/activity_indicator_cubit.dart';
 
-class CreateAccountPage extends StatelessWidget {
+class CreateAccountPage extends StatefulWidget {
   const CreateAccountPage({super.key});
+
+  @override
+  _CreateAccountPageState createState() => _CreateAccountPageState();
+}
+
+class _CreateAccountPageState extends State<CreateAccountPage> {
+  bool _isPasswordVisible = false;
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return AuthenticationLayout(
       children: [
-        Center(
-          child: Column(
-            children: [
-              const Text(
-                "Enter Personal Information",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+        const Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            "Enter Your Password",
+            style: TextStyle(fontSize: 18, color: Colors.white),
+          ),
+        ),
+        const SizedBox(height: 20),
+        TextField(
+          controller: _passwordController,
+          obscureText: !_isPasswordVisible,
+          style: const TextStyle(color: Colors.white),
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.black,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Colors.white),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Colors.white),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Colors.blue),
+            ),
+            suffixIcon: IconButton(
+              icon: Icon(
+                _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                color: Colors.white,
               ),
-              const SizedBox(height: 20),
-              Column(
-                children: [
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          CupertinoPageRoute(
-                            builder: (context) => const ForgotPasswordPage(),
-                          ),
-                        );
-                      },
-                      style: const ButtonStyle(
-                        padding: WidgetStatePropertyAll(EdgeInsets.all(10)),
-                        backgroundColor: WidgetStatePropertyAll(
-                          Color.fromARGB(213, 255, 255, 255),
-                        ),
+              onPressed: () {
+                setState(() {
+                  _isPasswordVisible = !_isPasswordVisible;
+                });
+              },
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        BlocBuilder<ActivityIndicatorCubit, bool>(
+          builder: (context, loading) {
+            return SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  BlocProvider.of<ActivityIndicatorCubit>(context).toggle();
+                  Future.delayed(const Duration(seconds: 2), () {
+                    BlocProvider.of<ActivityIndicatorCubit>(context).toggle();
+                    Navigator.push(
+                      context,
+                      CupertinoPageRoute(
+                        builder: (context) => const ForgotPasswordPage(),
                       ),
-                      child: const Text(
-                        "Continue",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 17,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
+                    );
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(15),
+                  backgroundColor: Colors.white,
+                ),
+                child: loading
+                    ? const CupertinoActivityIndicator(color: Colors.black)
+                    : const Text(
+                  "Sign In",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
                   ),
-                ],
+                ),
               ),
-            ],
+            );
+          },
+        ),
+        const SizedBox(height: 10),
+        SizedBox(
+          width: double.infinity,
+          child: TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                CupertinoPageRoute(
+                  builder: (context) => const ForgotPasswordPage(),
+                ),
+              );
+            },
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.grey[900],
+              padding: const EdgeInsets.all(15),
+            ),
+            child: const Text(
+              "Forgot Password",
+              style: TextStyle(
+                color: Colors.white60,
+                fontSize: 16,
+              ),
+            ),
           ),
         ),
       ],
     );
+  }
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    super.dispose();
   }
 }
