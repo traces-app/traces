376599c321637bbe62192ed30147c934b9e56c60
rashini <rashiniruwindee2001@gmail.com>
2025-01-31 14:50:43 +0530

    Updated Password Sign-In Page with visibility toggle and improved UI

--- lib/pages/authentication/views/password_page.dart
+++ lib/pages/authentication/views/password_page.dart
@@ -1,114 +1,118 @@
+import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:traces/pages/authentication/authentication.dart';
+import 'package:traces/pages/authentication/cubit/activity_indicator_cubit.dart';
+import 'package:traces/shared/widgets/styled_text_field.dart';
 
-class PasswordPage extends StatelessWidget {
+class PasswordPage extends StatefulWidget {
   const PasswordPage({super.key});
 
+  @override
+  State<PasswordPage> createState() => _PasswordPageState();
+}
+
+class _PasswordPageState extends State<PasswordPage> {
   @override
   Widget build(BuildContext context) {
     return AuthenticationLayout(
       children: [
-        Center(
-          child: Column(
-            children: [
-              const Text(
-                "Enter Password",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+        const Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            "Enter Your Password",
+            style: TextStyle(fontSize: 18, color: Colors.white),
+          ),
+        ),
+        const SizedBox(height: 20),
+
+        BlocBuilder<ActivityIndicatorCubit, bool>(builder: (context, state) {
+          return StyledTextField(
+            placeholder: "Password",
+            type: "password",
+            focused: true,
+            onSubmitted: (value) {
+              context.read<ActivityIndicatorCubit>().toggle();
+              Timer(
+                const Duration(seconds: 3),
+                    () {
+                  context.read<ActivityIndicatorCubit>().toggle();
+                  Navigator.push(
+                    context,
+                    CupertinoPageRoute(builder: (context) => const PoliciesPage()),
+                  );
+                },
+              );
+            },
+          );
+        }),
+        const SizedBox(height: 38),
+
+        // Sign In Button
+        BlocBuilder<ActivityIndicatorCubit, bool>(
+          builder: (context, loading) {
+            return SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white.withAlpha((0.9 * 255).toInt()),
+                  foregroundColor: Colors.black,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20.0,
+                    vertical: 11,
+                  ),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(18)),
+                  ),
+                ),
+                child: const Text(
+                  "Sign In",
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+                ),
+                onPressed: () {
+                  context.read<ActivityIndicatorCubit>().toggle();
+                  Timer(
+                    const Duration(seconds: 3),
+                        () {
+                      context.read<ActivityIndicatorCubit>().toggle();
+                      Navigator.push(
+                        context,
+                        CupertinoPageRoute(
+                          builder: (context) => const PoliciesPage(),
+                        ),
+                      );
+                    },
+                  );
+                },
               ),
-              const SizedBox(height: 20),
-              Column(
-                children: [
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          CupertinoPageRoute(
-                            builder: (context) => const PoliciesPage(),
-                          ),
-                        );
-                      },
-                      style: const ButtonStyle(
-                        padding: WidgetStatePropertyAll(EdgeInsets.all(10)),
-                        backgroundColor: WidgetStatePropertyAll(
-                          Color.fromARGB(213, 255, 255, 255),
-                        ),
-                      ),
-                      child: const Text(
-                        "Sign In",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 17,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          CupertinoPageRoute(
-                            builder: (context) => const VerificationPage(),
-                          ),
-                        );
-                      },
-                      style: const ButtonStyle(
-                        padding: WidgetStatePropertyAll(
-                          EdgeInsets.all(10),
-                        ),
-                        backgroundColor: WidgetStatePropertyAll(Colors.white12),
-                      ),
-                      child: const Text(
-                        "Forgot Password",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 17,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          CupertinoPageRoute(
-                            builder: (context) => const CreateAccountPage(),
-                          ),
-                        );
-                      },
-                      style: const ButtonStyle(
-                        padding: WidgetStatePropertyAll(
-                          EdgeInsets.all(10),
-                        ),
-                        backgroundColor: WidgetStatePropertyAll(Colors.white12),
-                      ),
-                      child: const Text(
-                        "Create Account",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 17,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  )
-                ],
+            );
+          },
+        ),
+        const SizedBox(height: 23),
+
+        // Forgot Password Button
+        SizedBox(
+          width: double.infinity,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.grey[900],
+              foregroundColor: Colors.white60,
+              padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 11),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(18)),
               ),
-            ],
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                CupertinoPageRoute(builder: (context) => const VerificationPage()),
+              );
+            },
+            child: const Text(
+              "Forgot Password",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+            ),
           ),
         ),
       ],
