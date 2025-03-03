25525c356c159b1b9ae8ced9a568ad68b6c553f4
DihanWanasinghe <dihan.20232101@iit.ac.lk>
2025-03-03 13:53:11 +0530

    Created A page with the functionality of changing user password

---
+++ lib/pages/authentication/views/change_password_page.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:traces/shared/widgets/PasswordStrengthDepiction.dart';
+import 'package:traces/shared/widgets/styled_text_field_form.dart';
+
+class ChangePasswordPage extends StatefulWidget {
+  ChangePasswordPage({super.key});
+
+  @override
+  State<ChangePasswordPage> createState() => _ChangePasswordPageState();
+}
+
+class _ChangePasswordPageState extends State<ChangePasswordPage> {
+
+  TextEditingController newPasswordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    newPasswordController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  String? validateNewPassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return "Please fill this field ";
+    } else if (value.length < 7) {
+      return "Password is too short";
+    } else if (!RegExp(r'^(?=.*[A-Z])(?=.*\d).+$').hasMatch(value)) {
+      return "One capital letter and number required";
+    }
+    return null;
+  }
+
+  String? validateConfirmPassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return "Please fill this field ";
+    } else if (newPasswordController.text != value) {
+      return "Password fields aren't matched";
+    }
+    return null;
+  }
+
+  void validateTextFields() {
+    if (_formKey.currentState!.validate()) {
+      print("Validated Successfully");
+    } else {
+      print("Invalid Values");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(Icons.arrow_back))
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(left: 24, right: 24),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Reset Your Password',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 17,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                StyledTextFormField(
+                  placeholder: "New Password",
+                  textEditingController: newPasswordController,
+                  validator: validateNewPassword,
+                  type: "password",
+                ),
+                if (newPasswordController.text.isNotEmpty) ...[
+                  SizedBox(height: 27),
+                  ContainerBar(newPasswordController: newPasswordController),
+                  SizedBox(height: 27),
+                ] else ...[
+                  SizedBox(height: 27),
+                ],
+                StyledTextFormField(
+                  placeholder: "Confirm New Password",
+                  textEditingController: confirmPasswordController,
+                  externalTextEditingController: newPasswordController,
+                  validator: validateConfirmPassword,
+                  type: "password",
+                ),
+                SizedBox(
+                  height: 41,
+                ),
+                ElevatedButton(
+                  onPressed: validateTextFields,
+                  child: Text(
+                    "Reset Password",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 17,
+                        color: Colors.black),
+                  ),
+                  style: ButtonStyle(
+                    padding: WidgetStatePropertyAll(EdgeInsets.all(10)),
+                    backgroundColor: WidgetStatePropertyAll(Color(0xFFD9D9D9)),
+                    shape: WidgetStatePropertyAll(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
