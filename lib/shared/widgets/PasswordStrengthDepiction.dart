e6793401d6279670c4f5e0dff95a34398efb58df
DihanWanasinghe <dihan.20232101@iit.ac.lk>
2025-03-03 13:49:33 +0530

    Created a collection of containers to depict the newly typed password strength

---
+++ lib/shared/widgets/PasswordStrengthDepiction.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ContainerBar extends StatefulWidget {
+  final TextEditingController newPasswordController;
+  ContainerBar({required this.newPasswordController, super.key});
+
+  @override
+  State<ContainerBar> createState() => _ContainerBarState();
+}
+
+class _ContainerBarState extends State<ContainerBar> {
+  List<Color> containerColors = List.generate(5, (index) => Color(0xFF7A7A7A));
+
+  @override
+  void initState() {
+    super.initState();
+    widget.newPasswordController.addListener(changingColors);
+  }
+
+  void changingColors() {
+    final text = widget.newPasswordController.text;
+    final length = text.length;
+    final hasUpperCase = RegExp(r'[A-Z]').hasMatch(text);
+    final hasNumber = RegExp(r'\d').hasMatch(text);
+
+    bool textLengthCheck6 = length > 6;
+    bool capsOrNumCheck = textLengthCheck6 && (hasUpperCase || hasNumber);
+    bool capsAndNumCheck = textLengthCheck6 && hasUpperCase && hasNumber;
+    bool textLengthCheck10 = capsAndNumCheck && length > 10;
+    bool textLengthCheck15 = textLengthCheck10 && length > 15;
+
+    List<Color> newColors = [
+      textLengthCheck6,
+      capsOrNumCheck,
+      capsAndNumCheck,
+      textLengthCheck10,
+      textLengthCheck15
+    ].map((condition) => condition ? Color(0xFFA8A972) : Color(0xFF7A7A7A)).toList();
+
+    if (newColors.toString() != containerColors.toString()) {
+      setState(() {
+        containerColors = newColors;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    changingColors();
+    return Container(
+      margin: EdgeInsets.only(top: 10, bottom: 20),
+      child: Row(
+        children: List.generate(5, (index) {
+          return Flexible(
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 5),
+              height: 5,
+              decoration: BoxDecoration(color: containerColors[index]),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
