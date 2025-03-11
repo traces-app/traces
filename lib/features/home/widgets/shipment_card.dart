60cc914385adf126d10323df92896db630102b4c
rashini <rashiniruwindee2001@gmail.com>
2025-03-11 11:25:13 +0530

    Add linking to shipment details

--- lib/features/home/widgets/shipment_card.dart
+++ lib/features/home/widgets/shipment_card.dart
@@ -4,6 +4,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:traces/core/models/group.dart';
 import 'package:traces/core/utils/app_icons.dart';
 import 'package:traces/shared/utils/markdown.dart';
+import 'package:traces/pages/shipment/views/shipment_details_page.dart'; // Import the details page
 
 class ShipmentCard extends StatelessWidget {
   final Group type;
@@ -27,131 +28,142 @@ class ShipmentCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 6.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    spacing: 3.0,
-                    children: [
-                      Row(
-                        spacing: 16.0,
-                        children: [
-                          Text(
-                            identifier,
-                            style: TextStyle(
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ShipmentDetailsPage(
+            ),
+          ),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 6.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              identifier,
+                              style: TextStyle(
                                 fontSize: 17.5,
                                 fontWeight: FontWeight.w600,
                                 letterSpacing: -0.5,
                                 color: (type == Group.canceled)
                                     ? Color(0xFFEDC9C9)
                                     : (type == Group.delivered)
-                                        ? Color(0xFFC3EEC2)
-                                        : Colors.white),
-                          ),
-                          Row(
-                            spacing: 6.0,
-                            children: [
-                              if (type != Group.delivered &&
-                                  type != Group.canceled)
-                                const Icon(
-                                  CupertinoIcons.calendar,
-                                  size: 19,
-                                  color: Color.fromRGBO(255, 255, 255, 0.7),
+                                    ? Color(0xFFC3EEC2)
+                                    : Colors.white,
+                              ),
+                            ),
+                            SizedBox(width: 16),
+                            Row(
+                              children: [
+                                if (type != Group.delivered &&
+                                    type != Group.canceled)
+                                  const Icon(
+                                    CupertinoIcons.calendar,
+                                    size: 19,
+                                    color: Color.fromRGBO(255, 255, 255, 0.7),
+                                  ),
+                                SizedBox(width: 6),
+                                Text(
+                                  timestamp,
+                                  style: TextStyle(
+                                    fontSize: 17.5,
+                                    fontWeight: FontWeight.w500,
+                                    letterSpacing: -0.5,
+                                    color: (type == Group.canceled)
+                                        ? Color.fromRGBO(237, 201, 201, 0.6)
+                                        : (type == Group.delivered)
+                                        ? Color.fromRGBO(195, 238, 194, 0.6)
+                                        : Color.fromRGBO(255, 255, 255, 0.7),
+                                  ),
                                 ),
-                              Text(
-                                timestamp,
-                                style: TextStyle(
-                                  fontSize: 17.5,
-                                  fontWeight: FontWeight.w500,
-                                  // color: Color.fromRGBO(255, 255, 255, 0.7),
-                                  letterSpacing: -0.5,
-                                  color: (type == Group.canceled)
-                                      ? Color.fromRGBO(237, 201, 201, 0.6)
-                                      : (type == Group.delivered)
-                                          ? Color.fromRGBO(195, 238, 194, 0.6)
-                                          : Color.fromRGBO(255, 255, 255, 0.7),
+                              ],
+                            ),
+                            if (type == Group.canceled || type == Group.delivered)
+                              SvgPicture.asset(
+                                type.iconPath!,
+                                height: 12.0,
+                                colorFilter: ColorFilter.mode(
+                                  type == Group.canceled
+                                      ? Color.fromRGBO(226, 76, 76, 0.7)
+                                      : Color.fromRGBO(51, 199, 90, 1),
+                                  BlendMode.srcIn,
                                 ),
                               ),
-                            ],
-                          ),
-                          if (type == Group.canceled || type == Group.delivered)
-                            SvgPicture.asset(
-                              type.iconPath!,
-                              height: 12.0,
-                              colorFilter: ColorFilter.mode(
-                                type == Group.canceled
-                                    ? Color.fromRGBO(226, 76, 76, 0.7)
-                                    : Color.fromRGBO(51, 199, 90, 1),
-                                BlendMode.srcIn,
+                            if (bubble)
+                              Container(
+                                width: 6,
+                                height: 6,
+                                decoration: BoxDecoration(
+                                  color: Color.fromRGBO(210, 101, 101, 0.32),
+                                  shape: BoxShape.circle,
+                                ),
+                              ),
+                          ],
+                        ),
+                        SizedBox(height: 3),
+                        Row(
+                          children: [
+                            Text(
+                              merchant,
+                              style: TextStyle(
+                                fontSize: 16.5,
+                                fontWeight: FontWeight.w500,
+                                letterSpacing: -0.3,
+                                color: (type == Group.canceled)
+                                    ? Color.fromRGBO(237, 201, 201, 0.6)
+                                    : (type == Group.delivered)
+                                    ? Color.fromRGBO(176, 219, 175, 0.6)
+                                    : Color.fromRGBO(255, 255, 255, 0.6),
                               ),
                             ),
-                          if (bubble)
-                            Container(
-                              width: 6,
-                              height: 6,
-                              decoration: BoxDecoration(
-                                color: Color.fromRGBO(210, 101, 101, 0.32),
-                                shape: BoxShape.circle,
+                            SizedBox(width: 12),
+                            Text(
+                              logistics,
+                              style: TextStyle(
+                                fontSize: 16.5,
+                                fontWeight: FontWeight.w500,
+                                letterSpacing: -0.3,
+                                color: (type == Group.canceled)
+                                    ? Color.fromRGBO(237, 201, 201, 0.6)
+                                    : (type == Group.delivered)
+                                    ? Color.fromRGBO(176, 219, 175, 0.6)
+                                    : Color.fromRGBO(255, 255, 255, 0.6),
                               ),
                             ),
-                        ],
-                      ),
-                      Row(
-                        spacing: 12.0,
-                        children: [
-                          Text(
-                            merchant,
-                            style: TextStyle(
-                              fontSize: 16.5,
-                              fontWeight: FontWeight.w500,
-                              letterSpacing: -0.3,
-                              color: (type == Group.canceled)
-                                  ? Color.fromRGBO(237, 201, 201, 0.6)
-                                  : (type == Group.delivered)
-                                      ? Color.fromRGBO(176, 219, 175, 0.6)
-                                      : Color.fromRGBO(255, 255, 255, 0.6),
-                            ),
-                          ),
-                          Text(
-                            logistics,
-                            style: TextStyle(
-                              fontSize: 16.5,
-                              fontWeight: FontWeight.w500,
-                              letterSpacing: -0.3,
-                              color: (type == Group.canceled)
-                                  ? Color.fromRGBO(237, 201, 201, 0.6)
-                                  : (type == Group.delivered)
-                                      ? Color.fromRGBO(176, 219, 175, 0.6)
-                                      : Color.fromRGBO(255, 255, 255, 0.6),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              SvgPicture.asset(
-                AppIcons.chevronRight,
-                height: 13.0,
-              ),
-            ],
-          ),
-          if (type == Group.inTransit && description != null)
-            Container(
-              margin: EdgeInsets.only(top: 12.0),
-              child: Markdown(description: description!),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SvgPicture.asset(
+                  AppIcons.chevronRight,
+                  height: 13.0,
+                ),
+              ],
             ),
-        ],
+            if (type == Group.inTransit && description != null)
+              Container(
+                margin: EdgeInsets.only(top: 12.0),
+                child: Markdown(description: description!),
+              ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/pages/shipment/views/shipment_details_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShipmentDetailsPage extends StatefulWidget {
+  const ShipmentDetailsPage({super.key});
+
+  @override
+  State<ShipmentDetailsPage> createState() => _ShipmentDetailsPageState();
+}
+
+class _ShipmentDetailsPageState extends State<ShipmentDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
