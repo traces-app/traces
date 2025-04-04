575685be347407d3fb0647e6c965c16a9ba6d51d
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2025-04-04 12:12:42 +0530

    implemented qr scan again using a different library

--- lib/pages/home/views/add_shipment/add_shipment_scan_view.dart
+++ lib/pages/home/views/add_shipment/add_shipment_scan_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:traces/shared/widgets/modal_bottom_sheet.dart';
 import 'package:traces/pages/home/views/add_shipment/add_shipment_manual_view.dart';
+import 'package:qr_code_scanner_plus/qr_code_scanner_plus.dart';
 
 class AddShipmentScanView extends StatelessWidget {
   const AddShipmentScanView({super.key});
@@ -18,7 +20,7 @@ class AddShipmentScanView extends StatelessWidget {
             children: [
               SizedBox(height: 6.0),
               TitleWithInstruction(),
-              SizedBox(height: 10),
+              SizedBox(height: 10.0),
               QRScanFinder(),
             ],
           ),
@@ -92,28 +94,73 @@ class TitleWithInstruction extends StatelessWidget {
   }
 }
 
-class QRScanFinder extends StatelessWidget {
+class QRScanFinder extends StatefulWidget {
   const QRScanFinder({super.key});
 
   @override
+  State<QRScanFinder> createState() => _QRScanFinderState();
+}
+
+class _QRScanFinderState extends State<QRScanFinder> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
+  String? scannedText;
+
+  @override
+  void initState() {
+    super.initState();
+    _requestCameraPermission();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {},
-      child: Container(
-        height: 280.0,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.circular(14.0),
+    return Container(
+      height: 280.0,
+      width: double.infinity,
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(14.0),
+      ),
+      child: QRView(
+        key: qrKey,
+        onQRViewCreated: _onQRViewCreated,
+        overlay: QrScannerOverlayShape(
+          borderColor: Colors.red,
+          borderRadius: 10,
+          borderLength: 30,
+          borderWidth: 10,
+          cutOutSize: 180,
         ),
-        child: const Center(
+      ),
+    );
+  }
+
+  void _requestCameraPermission() async {
+    var status = await Permission.camera.status;
+    if (!status.isGranted) {
+      status = await Permission.camera.request();
+    }
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        scannedText = scanData.code;
+        print(scannedText);
+      });
+    });
+  }
+}
+
+/*
+const Center(
           child: Icon(
             CupertinoIcons.qrcode_viewfinder,
             size: 180.0,
             color: Colors.white30,
           ),
         ),
-      ),
-    );
-  }
-}
+
+ */
