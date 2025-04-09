4058e9823eb03626e37ab1b22502c3c67812b76b
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2025-04-09 13:48:59 +0530

    cleanup

--- lib/shared/widgets/shipment details/shipment_status_titles.dart
+++
@@ -1,9 +0,0 @@
-enum ShipmentStatus {
-  shippingAddress("Shipping Address"),
-  delivered("Delivered"),
-  outForDelivery("Out for Delivery"),
-  pickedUp("Picked Up");
-
-  final String value;
-  const ShipmentStatus(this.value);
-}
