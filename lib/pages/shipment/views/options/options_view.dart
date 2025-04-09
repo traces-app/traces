b9ecd844bfd4756f44f4d3536c5ac3051d56d5fb
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2025-04-10 01:33:46 +0530

    created the option view of menu items

--- lib/pages/shipment/views/options/options_view.dart
+++ lib/pages/shipment/views/options/options_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:traces/pages/map/views/map_page.dart';
 import 'package:traces/pages/shipment/views/edit/view_shipping_info_view.dart';
 import 'package:traces/pages/shipment/views/returns/eligibility_view.dart';
@@ -11,51 +12,103 @@ class OptionsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: Start replacing content from here
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 26.0),
-          child: Column(
-            children: [
-              ElevatedButton(
-                onPressed: () async {
-                  final rootContext = Navigator.of(context, rootNavigator: true)
-                      .context; // capture safe context
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        spacing: 30.0,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12.0),
+            child: Column(
+              spacing: 1.0,
+              children: [
+                CupertinoContextMenuAction(
+                  trailingIcon: CupertinoIcons.map_pin,
+                  onPressed: () async {
+                    final rootContext =
+                        Navigator.of(context, rootNavigator: true)
+                            .context; // capture safe context
 
-                  Navigator.of(context, rootNavigator: true).pop();
-                  await Future.delayed(
-                      Duration(milliseconds: 500)); // Add delay
-                  if (rootContext.mounted) {
-                    Navigator.of(rootContext).push(
-                      CupertinoPageRoute(builder: (context) => MapPage()),
-                    );
-                  }
-                },
-                child: Text("Map View"),
-              ),
-              SizedBox(height: 5.0), // Spacing between buttons
-              ElevatedButton(
-                onPressed: () {
-                  final modal =
-                      context.findAncestorStateOfType<ModalBottomSheetState>();
-                  modal?.navigateTo(ViewShippingInformationView());
-                },
-                child: Text("Edit Shipping Address"),
-              ),
-              SizedBox(height: 5.0), // Spacing between buttons
-              ElevatedButton(
-                onPressed: () {
-                  final modal =
-                      context.findAncestorStateOfType<ModalBottomSheetState>();
-                  modal?.navigateTo(EligibilityView());
-                },
-                child: Text("Report a Return"),
-              ),
-            ],
+                    Navigator.of(context, rootNavigator: true).pop();
+                    await Future.delayed(
+                        Duration(milliseconds: 500)); // Add delay
+                    if (rootContext.mounted) {
+                      Navigator.of(rootContext).push(
+                        CupertinoPageRoute(builder: (context) => MapPage()),
+                      );
+                    }
+                  },
+                  child: Text("Map View", style: TextStyle(fontSize: 17.5)),
+                ),
+                CupertinoContextMenuAction(
+                  trailingIcon: CupertinoIcons.keyboard,
+                  onPressed: () {
+                    final modal = context
+                        .findAncestorStateOfType<ModalBottomSheetState>();
+                    modal?.navigateTo(ViewShippingInformationView());
+                  },
+                  child: Text("Edit Shipping Address",
+                      style: TextStyle(fontSize: 17.5)),
+                ),
+                CupertinoContextMenuAction(
+                  trailingIcon: CupertinoIcons.arrow_counterclockwise,
+                  onPressed: () {
+                    final modal = context
+                        .findAncestorStateOfType<ModalBottomSheetState>();
+                    modal?.navigateTo(EligibilityView());
+                  },
+                  child: Text("Request a Return",
+                      style: TextStyle(fontSize: 17.5)),
+                ),
+                CupertinoContextMenuAction(
+                  trailingIcon: CupertinoIcons.map_pin,
+                  onPressed: () {},
+                  child: Text("Report a Problem",
+                      style: TextStyle(fontSize: 17.5)),
+                ),
+                CupertinoContextMenuAction(
+                  trailingIcon: CupertinoIcons.chat_bubble,
+                  onPressed: () {},
+                  child: Text(
+                    "Contact Customer Service",
+                    style: TextStyle(fontSize: 17.5),
+                  ),
+                ),
+                CupertinoContextMenuAction(
+                  isDestructiveAction: true,
+                  trailingIcon: CupertinoIcons.xmark_circle,
+                  onPressed: () {},
+                  child: Text(
+                    "Cancel Order",
+                    style: TextStyle(
+                      fontSize: 17.5,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12.0),
+            child: Column(
+              children: [
+                CupertinoContextMenuAction(
+                  isDestructiveAction: true,
+                  trailingIcon: CupertinoIcons.delete,
+                  onPressed: () {},
+                  child: Text(
+                    "Delete Shipment",
+                    style: TextStyle(
+                      fontSize: 17.5,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
