4417c3b042f5e88440ef7b5a43cf9c26100f3d06
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2025-04-09 07:18:38 +0530

    added final ui touch modifications

--- lib/shared/widgets/order_confirmation_view.dart
+++ lib/shared/widgets/order_confirmation_view.dart
@@ -1,151 +1,7 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter/cupertino.dart';
-
-// class OrderConfirmationView extends StatelessWidget {
-//   const OrderConfirmationView({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: const EdgeInsets.only(
-//         top: 38.0,
-//         left: 16.0,
-//         right: 16.0,
-//         bottom: 16.0,
-//       ),
-//       child: Column(
-//         mainAxisSize: MainAxisSize.min,
-//         crossAxisAlignment: CrossAxisAlignment.center,
-//         children: [
-//           const _SuccessIcon(),
-//           const SizedBox(height: 22),
-//           const _DateTimeRow(),
-//           const SizedBox(height: 15),
-//           const _ShipmentDetailText(),
-//           const SizedBox(height: 22),
-//           const _CustomDivider(),
-//           _buildRatingRow("Amazon.com, Inc"),
-//           const _CustomDivider(),
-//           _buildRatingRow("FedEx Corporation"),
-//           const _CustomDivider(),
-//         ],
-//       ),
-//     );
-//   }
-
-//   Widget _buildRatingRow(String companyName) {
-//     return Padding(
-//       padding: const EdgeInsets.symmetric(vertical: 15.0),
-//       child: Row(
-//         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//         children: [
-//           Text(
-//             companyName,
-//             style: const TextStyle(
-//               fontSize: 19.0,
-//               color: Colors.grey,
-//             ),
-//           ),
-//           Row(
-//             children: List.generate(5, (index) {
-//               return Padding(
-//                 padding: const EdgeInsets.symmetric(horizontal: 8.0),
-//                 child: const Icon(
-//                   CupertinoIcons.star,
-//                   size: 22,
-//                   color: Color.fromRGBO(0, 122, 255, 1),
-//                 ),
-//               );
-//             }),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// class _SuccessIcon extends StatelessWidget {
-//   const _SuccessIcon();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Icon(
-//       CupertinoIcons.check_mark_circled,
-//       color: Color.fromRGBO(4, 144, 41, 1),
-//       size: 53.0,
-//     );
-//   }
-// }
-
-// class _DateTimeRow extends StatelessWidget {
-//   const _DateTimeRow();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Row(
-//       mainAxisAlignment: MainAxisAlignment.center,
-//       children: const [
-//         Icon(
-//           CupertinoIcons.calendar,
-//           size: 19,
-//           color: Colors.grey,
-//         ),
-//         SizedBox(width: 6),
-//         Text(
-//           "Thu Jan 30  09:41 PM",
-//           style: TextStyle(fontSize: 17.15, color: Colors.grey),
-//         ),
-//       ],
-//     );
-//   }
-// }
-
-// class _ShipmentDetailText extends StatelessWidget {
-//   const _ShipmentDetailText();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       width: 300,
-//       child: RichText(
-//         textAlign: TextAlign.center,
-//         text: const TextSpan(
-//           style: TextStyle(
-//             color: Color.fromRGBO(255, 255, 255, 0.7),
-//             fontSize: 18.0,
-//             height: 1.5,
-//           ),
-//           children: [
-//             TextSpan(text: "Your Shipment "),
-//             TextSpan(
-//               text: "TX 768 431",
-//               style: TextStyle(
-//                 color: Colors.white,
-//               ),
-//             ),
-//             TextSpan(text: " Has Been Delivered Successfully."),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// class _CustomDivider extends StatelessWidget {
-//   const _CustomDivider();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Divider(
-//       height: 0,
-//       thickness: 0.5,
-//       color: Color.fromRGBO(84, 84, 88, 0.65),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:traces/core/utils/app_icons.dart';
 
 class OrderConfirmationView extends StatefulWidget {
   const OrderConfirmationView({super.key});
@@ -182,9 +38,9 @@ class _OrderConfirmationViewState extends State<OrderConfirmationView> {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           const _SuccessIcon(),
-          const SizedBox(height: 22),
+          const SizedBox(height: 30.0),
           const _DateTimeRow(),
-          const SizedBox(height: 15),
+          const SizedBox(height: 13.0),
           const _ShipmentDetailText(),
           const SizedBox(height: 22),
           const _CustomDivider(),
@@ -199,14 +55,14 @@ class _OrderConfirmationViewState extends State<OrderConfirmationView> {
 
   Widget _buildRatingRow(String companyName, int currentRating) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 15.0),
+      padding: const EdgeInsets.symmetric(vertical: 13.5, horizontal: 5.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text(
             companyName,
             style: const TextStyle(
-              fontSize: 19.0,
+              fontSize: 18.5,
               color: Colors.grey,
             ),
           ),
@@ -239,11 +95,15 @@ class _SuccessIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Icon(
-      CupertinoIcons.check_mark_circled,
-      color: Color.fromRGBO(4, 144, 41, 1),
-      size: 53.0,
-    );
+    return Center(
+        child: SvgPicture.asset(
+      height: 45.0,
+      AppIcons.checkmark,
+      colorFilter: ColorFilter.mode(
+        Color(0xff049029),
+        BlendMode.srcIn,
+      ),
+    ));
   }
 }
 
@@ -254,16 +114,20 @@ class _DateTimeRow extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: const [
+      children: [
         Icon(
           CupertinoIcons.calendar,
-          size: 19,
-          color: Colors.grey,
+          size: 18,
+          color: Colors.white.withOpacity(0.5),
         ),
         SizedBox(width: 6),
         Text(
-          "Thu Jan 30  09:41 PM",
-          style: TextStyle(fontSize: 17.15, color: Colors.grey),
+          "Thu Jan 30 09:41 PM",
+          style: TextStyle(
+            fontSize: 17.0,
+            color: Colors.white.withOpacity(0.5),
+            fontWeight: FontWeight.w500,
+          ),
         ),
       ],
     );
@@ -276,25 +140,28 @@ class _ShipmentDetailText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 300,
+      padding: EdgeInsets.symmetric(
+        horizontal: 6.0,
+      ),
       child: RichText(
         textAlign: TextAlign.center,
-        text: const TextSpan(
-          style: TextStyle(
-            color: Color.fromRGBO(255, 255, 255, 0.7),
-            fontSize: 18.0,
-            height: 1.5,
-          ),
-          children: [
-            TextSpan(text: "Your Shipment "),
+        text: TextSpan(
+          text: "Your Shipment",
+          children: const [
             TextSpan(
-              text: "TX 768 431",
+              text: ' TX 768 431 ',
               style: TextStyle(
                 color: Colors.white,
               ),
             ),
-            TextSpan(text: " Has Been Delivered Successfully."),
+            TextSpan(text: 'Has Been Delivered Successfully.'),
           ],
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w500,
+            color: Colors.white.withOpacity(0.85),
+            height: 1.5,
+          ),
         ),
       ),
     );
