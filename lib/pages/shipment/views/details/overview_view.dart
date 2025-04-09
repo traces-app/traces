7602dd5590ff1a450692e3b81c9c9d87ecf61bd4
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2025-04-09 09:13:53 +0530

    refined the shipping address view section in the overview section of the shipment details page

--- lib/pages/shipment/views/details/overview_view.dart
+++ lib/pages/shipment/views/details/overview_view.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:traces/shared/widgets/status_icon.dart';
 
 class OverviewView extends StatefulWidget {
   const OverviewView({super.key});
@@ -11,9 +13,87 @@ class _OverviewViewState extends State<OverviewView> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red.withOpacity(0.3),
-      child: Center(
-        child: Text('Overview'),
+      color: Colors.black,
+      child: Padding(
+        padding: const EdgeInsets.only(
+          top: 25.0,
+          left: 15.0,
+          right: 15.0,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              spacing: 10.0,
+              children: [
+                StatusIcon(type: "transit"),
+                Text(
+                  "In Transit",
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    fontSize: 17.5,
+                    letterSpacing: 0.15,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white.withOpacity(0.9),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 30.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Shipping Address",
+                          style: TextStyle(
+                            fontSize: 17.5,
+                            letterSpacing: 0.15,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white.withOpacity(0.6),
+                          ),
+                        ),
+                        SizedBox(height: 4.0),
+                        Text(
+                          "No. 25, Dambulla Road, Kurunegala, North Western, 60000",
+                          style: TextStyle(
+                            fontSize: 17.5,
+                            letterSpacing: 0.15,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white.withOpacity(0.85),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    padding: EdgeInsets.all(3.0),
+                    decoration: BoxDecoration(
+                      color: Color(0xff222222),
+                      shape: BoxShape.circle,
+                    ),
+                    child: Transform.rotate(
+                      angle: -0.9, // radians
+                      child: Icon(
+                        CupertinoIcons.arrow_up,
+                        size: 20.0,
+                        color: Colors.white.withOpacity(0.5),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
